249c055a5c09c2080a454af8cf03d965c78e59f2
Lauren <mlmxwell@gmail.com>
2016-07-24 18:02:30 -0500

    Adds starfield image and alters some of the backend.

--- server.js
+++ server.js
@@ -1,22 +1,19 @@
-// server.js
-
 // modules =================================================
 var express = require('express');
 var app = express();
 var Twit = require('twit');
-
-app.set('view engine', 'ejs');
-
-// configuration ===========================================
-
-// set our port
-var port = process.env.PORT || 3000;
-
 // Setup server
 var server = require('http').Server(app);
 
 // IO
-var io = require('socket.io')(server);
+var io = require('socket.io')(server, {'multiplex': false});
+
+// app.set('view engine', 'ejs');
+
+// configuration ===========================================
+
+// set our port
+var port = process.env.PORT || 3030;
 
 // Start server
 server.listen(port, function () {
@@ -31,24 +28,34 @@ app.get('/', function (req, res) {
 });
 
 // Socket ==================================================
-io.on('connection', function (socket) {
+io.sockets.on('connection', function (socket) {
+  console.log(socket);
   var twit = new Twit({
     consumer_key:         '6U3ASBeodwSY8OgrNg4diORnJ',
     consumer_secret:      'wCL3DgE0QMWobbhpALWpgx83pHn0taUdmKKLfXjdALs0kbs9Ji',
     access_token:         '752643629978488835-rrT8yDAspH4gHxTJkBT1qHS4bVSWs3M',
-    access_token_secret:  'sho2o1YkyOEMWmIARuLHMEth3yVrTRpViQ8QHCXo0Z3ms',
-    timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+    access_token_secret:  'sho2o1YkyOEMWmIARuLHMEth3yVrTRpViQ8QHCXo0Z3ms'
   });
   var stream = twit.stream('statuses/filter', { track: ['@SimonHi'] });
+  console.log(stream);
 
   stream.on('tweet', function(tweet) {
     console.log(tweet);
+    if (tweet.entities.media) {
+      socket.emit('tweetmedia', { message: tweet.entities.media[0] });
+      console.log(tweet.entities.media[0]);
+    }
+
     socket.emit('tweet', { message: tweet.text });
   });
 
   
   socket.on('my other event', function (data) {
     console.log(data);
+  });
+
+  stream.on('disconnect', function (disconnectMessage) {
+    console.log(disconnectMessage);
   });
 });
 
