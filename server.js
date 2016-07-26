2e3cceb7d74cf3cd1d8e26461f558c9bb7754b7e
Lauren <mlmxwell@gmail.com>
2016-07-26 00:34:11 -0500

    Finally working on client reconnect doh'.

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ var express = require('express');
 var app = express();
 var Twit = require('twit');
 // Setup server
-var server = require('http').Server(app);
+var server = require('http').createServer(app);
 
 // IO
-var io = require('socket.io')(server, {'multiplex': false});
+var io = require('socket.io')(server);
 
 // app.set('view engine', 'ejs');
 
@@ -15,6 +15,10 @@ var io = require('socket.io')(server, {'multiplex': false});
 // set our port
 var port = process.env.PORT || 3001;
 
+// Start server
+server.listen(port, function () {
+  console.log('Express server listening on %d', port);
+});
 
 // routes ==================================================
 app.use('/', express.static('app'));
@@ -25,48 +29,44 @@ app.get('/', function (req, res) {
 
 var tweetText;
 
+var twit = new Twit({
+  consumer_key:         '6U3ASBeodwSY8OgrNg4diORnJ',
+  consumer_secret:      'wCL3DgE0QMWobbhpALWpgx83pHn0taUdmKKLfXjdALs0kbs9Ji',
+  access_token:         '752643629978488835-rrT8yDAspH4gHxTJkBT1qHS4bVSWs3M',
+  access_token_secret:  'sho2o1YkyOEMWmIARuLHMEth3yVrTRpViQ8QHCXo0Z3ms'
+});
+var stream = twit.stream('statuses/filter', { track: ['@SimonHi'] });
+stream.on('tweet', function(tweet) {
+  console.log(tweet);
+  if (tweet.entities.media) {
+    io.emit('tweetmedia', { message: tweet.entities.media[0] });
+    console.log(tweet.entities.media[0]);
+
+    tweetText = tweet.text.replace(tweet.entities.media[0].url, '');
+    console.log(tweetText);
+  }
+  else {
+    tweetText = tweet.text;
+  }
+
+  io.emit('tweet', { message: tweetText });
+});
+
+
+
 // Socket ==================================================
 io.on('connection', function (socket) {
   console.log(socket);
-  var twit = new Twit({
-    consumer_key:         '6U3ASBeodwSY8OgrNg4diORnJ',
-    consumer_secret:      'wCL3DgE0QMWobbhpALWpgx83pHn0taUdmKKLfXjdALs0kbs9Ji',
-    access_token:         '752643629978488835-rrT8yDAspH4gHxTJkBT1qHS4bVSWs3M',
-    access_token_secret:  'sho2o1YkyOEMWmIARuLHMEth3yVrTRpViQ8QHCXo0Z3ms'
-  });
-  var stream = twit.stream('statuses/filter', { track: ['@SimonHi'] });
+  
   console.log(stream);
 
-  stream.on('tweet', function(tweet) {
-    console.log(tweet);
-    if (tweet.entities.media) {
-      socket.emit('tweetmedia', { message: tweet.entities.media[0] });
-      console.log(tweet.entities.media[0]);
 
-      tweetText = tweet.text.replace(tweet.entities.media[0].url, '');
-      console.log(tweetText);
-    }
-    else {
-      tweetText = tweet.text;
-    }
-
-
-    socket.emit('tweet', { message: tweetText });
-  });
-
-  
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
 
   stream.on('disconnect', function (disconnectMessage) {
     console.log(disconnectMessage);
   });
 });
-// Start server
-server.listen(port, function () {
-  console.log('Express server listening on %d', port);
-});
+
 
 
 // expose app
