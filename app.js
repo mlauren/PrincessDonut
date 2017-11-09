78de239e518241108e2833ff61d0dacccdb72636
Lauren <mlmxwell@gmail.com>
2017-11-08 18:48:37 -0600

    Adds app and index update.

--- app.js
+++ app.js
@@ -87,7 +87,7 @@ stream.on('tweet', function(tweet) {
 if (module === require.main) {
   // [START server]
   // Start the server
-  server.listen(process.env.PORT || 8081, () => {
+  server.listen(process.env.PORT || 8080, () => {
     const port = server.address().port;
     console.log(`App listening on port ${port}`);
   });
